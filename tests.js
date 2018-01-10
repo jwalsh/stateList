e47e60a05a20d1e5d0434ebdb71efed0cbfdbbdd
Jason Walsh <jwalsh@saylent.com>
2018-01-10 11:15:58 -0500

    Test the name lookup

--- tests.js
+++ tests.js
@@ -1,4 +1,9 @@
 let states = require('./index');
 
+var abbr = require( 'datasets-us-states-abbr' );
+var _ = require('lodash');
+
 console.log(states.name['MA']);
 console.log(states.abbrev['Washington']);
+
+console.log(states.name[_.sample(abbr)]);
